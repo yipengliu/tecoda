3921349f265e054fc65c4e432357ab8be3497e97
owt <3178269754@qq.com>
2022-12-16 21:51:20 +0800

    test cpclass succussfully

--- @cptensor/cp2tensor.m
+++ @cptensor/cp2tensor.m
@@ -1,16 +1,17 @@
 function T = cp2tensor(CP)
 %cp2tensor transform a CP format tensor into a dense tensor
 
-    minS=minSplit(CP.shape);
-    if (minS==length(CP.shape))
-        data = khatrirao(CP.factors,'r') * CP.weights;
+    minS=minSplit(CP.sz);
+    if (minS==length(CP.sz))
+        data = khatrirao_product(CP.factors) * CP.weights;
     else
         % This unrolls modes 1:minS into rows and minS+1:end into columns
         % of the column-major matrix data which is then converted into a
         % tensor without permutation.
         data = khatrirao(CP.factors(1:minS),'r') * diag(CP.weights) * khatrirao(CP.factors(minS+1:end),'r').';
+        %data = khatrirao_product(CP.factors(1:minS)) * diag(CP.weights) * khatrirao_product(CP.factors(minS+1:end)).';
     end
-    T = tensor(data,CP.shape);
+    T = tensor(data,CP.sz);
 end
 
 function [minS]=minSplit(sz)
