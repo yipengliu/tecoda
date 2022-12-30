670b958905aadddf488ba6bd2bc98baec9703849
liujiani <Jianiliunier@gmail.com>
2022-12-30 17:59:30 +0800

    Update test_cp.m

--- test_cp.m
+++ test_cp.m
@@ -110,7 +110,56 @@ X_hat=cp2tensor(T);
 
 Err=norm(calculate("minus",X,cp2tensor(T)))/norm(X);%% needs further define
 
-fprintf("CP approxiamtion with rank %d achieve approxiamtion erro %d.\n",R,Err)
+fprintf("CP approximation with rank %d achieve approximation erro %d.\n",R,Err)
+
+
+if Qpause
+    fprintf("Enter any key to continue,press ctrl+c to exit\n")
+    pause
+end
+
+
+%% test cp_als with real image
+clc
+disp('%%%%%%%%test cp_als %%%%%%%%');
+
+X=double(imread('sherlock.jpg'));
+X=tensor(X);
+% rank : 10
+R=10;
+T=cp_als(X,R);
+X1 = cp2tensor(T);
+Err1 = norm(calculate("minus", X1, X));
+nx=norm(X);
+
+fprintf("CP approximation with rank %d achieve approximation error %d, relative error %d.\n",R,Err1,Err1/nx);
+
+
+% rank : 50
+R=50;
+T=cp_als(X,R);
+X2 = cp2tensor(T);
+Err3 = norm(calculate("minus", X2, X));
+nx=norm(X);
+
+fprintf("CP approximation with rank %d achieve approximation error %d, relative error %d.\n",R,Err2,Err2/nx);
+
+
+% disply results with cp_als
+figure;
+set(gcf,'unit','centimeters','position',[10 10 30 10]);
+
+subplot(1,3,1);
+imshow(imread('sherlock.jpg'));
+title('Origin image','fontsize',12);
+
+subplot(1,3,2);
+imshow(uint8(double(X1)));
+title({['rank=10'];['AE=', num2str(Err1)]},'fontsize',12);
+
+subplot(1,3,3);
+imshow(uint8(double(X2)));
+title({['rank=50'];['AE=', num2str(Err2)]},'fontsize',12);
 
 
 if Qpause
