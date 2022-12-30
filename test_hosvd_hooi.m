4a275ba19c10db3718490cd379721e64d0b1384d
liujiani <Jianiliunier@gmail.com>
2022-12-30 18:00:06 +0800

    Update and rename test_hosvd_hooi.m to test_tk.m

--- test_hosvd_hooi.m
+++
@@ -1,8 +0,0 @@
-X = tensor(rand(5,6,7,8));
-rank = [2,2,2,2];
-y1 = hosvd(X,rank);
-y2 = HOOI(X,rank);
-Y1 = TK2tensor(y1);
-Y2 = TK2tensor(y2);
-error1 = norm(calculate("minus", Y1, X));
-error2 = norm(calculate("minus", Y2, X));
