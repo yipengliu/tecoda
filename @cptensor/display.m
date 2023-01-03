55d9c32ce52994bb04512e3c522e31d651e078e9
ouweiting <ouweiting7@gmail.com>
2023-01-03 17:00:38 +0800

    fix the bug of displaying in former matlab version

--- @cptensor/display.m
+++ @cptensor/display.m
@@ -2,12 +2,12 @@ function message = display(T)
 %report the meassage of CP format tensor
     if T.rank
         fprintf(['%s is a rank-%d CPtensor of shape',repmat(' %d',1,numel(T.size)), '.\n'], getVarName(T,inputname(1)),T.rank,T.size(:));
-        fprintf(['The weights of each columns in factor matrices is ',repmat(' %g',1,numel(T.weights)), '.\n'], T.weights(:));
+        fprintf(['The weights of each columns in factor matrices are ',repmat(' %g',1,numel(T.weights)), '.\n'], T.weights(:));
         disp("The size of factor matrices:");
         sz = [T.size;repmat(T.rank,1,numel(T.size))];
         colname = {};
         for i = 1:numel(T.size)
-            colname{1,i} = char(string(i));
+            colname{1,i} = char(regexprep(string(i),'^(?![a-z])', 'f', 'emptymatch', 'ignorecase'));
         end
         tb = array2table(sz,'VariableNames',colname,'RowName',{'length','width'});
         disp(tb);
