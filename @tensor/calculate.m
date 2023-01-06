09c73ed8eb043455d34593ee2d4b249778a90d86
wuke <1149526098@qq.com>
2023-01-06 11:59:12 +0800

    some modifications on tensor and calculation

--- @tensor/calculate.m
+++ @tensor/calculate.m
@@ -3,11 +3,11 @@ function T = calculate(op,A,B)
 if ~isa(A,'tensor')||~isa(B,'tensor')
     error('input must be tensor');
 else
-    count = tensor("zeros",size(A));
-    if(op=="add")
+    count = tensor('zeros',size(A));
+    if(op=='add')
         count.data = A.data+B.data;
     end
-    if(op=="minus")
+    if(op=='minus')
         count.data = A.data-B.data;
     end
     T=count;
