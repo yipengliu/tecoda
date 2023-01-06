4f8851f67cea0f48855cf5e6331f6f157682bc40
liujiani <Jianiliunier@gmail.com>
2023-01-06 15:33:11 +0800

    Update test_op.m

--- optimization/solver/test_op.m
+++ optimization/solver/test_op.m
@@ -5,7 +5,7 @@
 %example 1 
 % min 0.5*||QA-A||_F^2 s.t. Q^T*Q=I
 % solution: Q=I
-
+clear all
 disp('%%%%%%%%%%%%Exp 1%%%%%%%%%%%%%%%%%');
 
 disp(' min 0.5*||QA-A||_F^2 s.t. Q^T*Q=I');
@@ -14,13 +14,17 @@ A=rand(5,4);
 disp('A=');
 disp(A)
 disp('get the orthogonal solution through Q=op(A,A,0.5);')
-Q=op(A,A,0.5);
+Q=op(A,A);
 disp('The solution is ');
 disp('Q=');
 disp(Q)
 if norm(Q-eye(size(Q)),'fro')<1e-10
     disp('The solution is an identity matrix');
 end
+if norm(Q'*Q-eye(size(Q)),'fro')<1e-3
+    disp('The estimated Q is orthognal !')
+end
+disp(['The approximate error is ',num2str(norm(Q*A-A,'fro'))]);
 
 
 %example 2
@@ -36,7 +40,7 @@ X=rand(5,5);
 disp('Q=');
 disp(Q)
 
-if norm(Q'*Q-eye(size(Q)),'fro')<1e-10
+if norm(Q'*Q-eye(size(Q)),'fro')<1e-3
     disp('The generated Q is orthognal !')
 end
 disp('Genarate a random matrix A')
@@ -52,9 +56,14 @@ Q_hat=op(A,B,0.5);
 disp('The solution is ');
 disp('Q_hat=');
 disp(Q_hat)
-if norm(Q-Q_hat,'fro')<1e-10
+if norm(Q-Q_hat,'fro')<1e-3
     disp('The solution is what we generated !!!');
 end
+if norm(Q_hat'*Q_hat-eye(size(Q)),'fro')<1e-3
+    disp('The estimated Q is orthognal !')
+end
+disp(['The approximate error is ',num2str(norm(Q_hat*A-B,'fro'))]);
+
 %% test Orthogonal Procrustes Problem with several objective functions
 clear all;
 disp('%%%%%%%%%%%%Exp 3%%%%%%%%%%%%%%%%%');
@@ -66,7 +75,7 @@ X=rand(5,5);
 disp('Q=');
 disp(Q)
 
-if norm(Q'*Q-eye(size(Q)),'fro')<1e-10
+if norm(Q'*Q-eye(size(Q)),'fro')<1e-3
     disp('The generated Q is orthognal !')
 end
 disp('Genarate random matrices A{1},..., A{N}')
@@ -89,6 +98,14 @@ Q_hat=op(A,B,rho);
 disp('The solution is ');
 disp('Q_hat=');
 disp(Q_hat)
-if norm(Q-Q_hat,'fro')<1e-10
+if norm(Q-Q_hat,'fro')<1e-3
     disp('The solution is what we generated !!!');
 end
+if norm(Q_hat'*Q_hat-eye(size(Q)),'fro')<1e-3
+    disp('The estimated Q is orthognal !')
+end
+err=0;
+for n=1:N
+  err=err+norm(Q_hat*A{n}-B{n},'fro');   
+end
+disp(['The approximate error is ',num2str(err)]);
