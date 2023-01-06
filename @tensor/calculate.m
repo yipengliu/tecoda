ab3043816418ed96a6812d10185b713c48f72374
liujiani <Jianiliunier@gmail.com>
2023-01-06 15:51:33 +0800

    Update calculate.m

--- @tensor/calculate.m
+++ @tensor/calculate.m
@@ -4,11 +4,13 @@ if ~isa(A,'tensor')||~isa(B,'tensor')
     error('input must be tensor');
 else
     count = tensor('zeros',size(A));
-    if(op=='add')
-        count.data = A.data+B.data;
-    end
-    if(op=='minus')
-        count.data = A.data-B.data;
+    switch op
+        case 'add'
+            count.data = A.data+B.data;
+        case 'minus'
+            count.data = A.data-B.data;
+        otherwise
+            error('The given operation is not supported !!! please use add or minus');
     end
     T=count;
     return;
