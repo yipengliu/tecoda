92ec3412c0c5baad236a4c09eb352e753c879e00
liujiani <Jianiliunier@gmail.com>
2023-11-27 20:34:07 +0800

    Update TR2tensor.m

--- td/TRtensor/@TRtensor/TR2tensor.m
+++ td/TRtensor/@TRtensor/TR2tensor.m
@@ -1,10 +1,10 @@
 function t=TR2tensor(TR)
-    %convert a TRtensor to a tensor.
-    N = length(TR.factors);
-    t = TR.factors{1};
-    for i = 1:N-2
-        t = tensor_contraction(t,TR.factors{i+1},ndims(t),1);
-    end
-    t = tensor_contraction(t,TR.factors{N},[1, ndims(t)],[3, 1]);
-    t = tensor(t);
+%convert a TRtensor to a tensor.
+N = length(TR.factors);
+t = TR.factors{1};
+for i = 1:N-2
+    t = tensor_contraction(t,TR.factors{i+1},i+2,1);
+end
+t = tensor_contraction(t,TR.factors{N},[1, N+1],[3, 1]);
+t = tensor(t);
 end
