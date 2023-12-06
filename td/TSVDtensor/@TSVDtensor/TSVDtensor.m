0336f8ca37215370ac4ec2dc8446e01d8ff738a5
Weiting OU <47353652+ouweiting@users.noreply.github.com>
2023-12-06 20:59:44 +0800

    Update TSVDtensor.m

--- td/TSVDtensor/@TSVDtensor/TSVDtensor.m
+++ td/TSVDtensor/@TSVDtensor/TSVDtensor.m
@@ -1,28 +1,33 @@
 classdef TSVDtensor
     properties
-        size
         U
         S
         V
     end
     methods
         function TSVD = TSVDtensor(varargin)
-            if nargin==1 && (isa(varargin{1},'tensor') || isnumeric(varargin{1}))
-                [TSVD.U, TSVD.S, TSVD.V] = t_SVD(varargin{1},'p');
-                if isa(varargin{1},'tensor')
-                    TSVD.size = varargin{1}.size;
-                else
-                    TSVD.size = size(varargin{1});
-                end
-            elseif nargin==2 && (isa(varargin{1},'tensor')|| isnumeric(varargin{1})) && (varargin{2}=='p' || varargin{2}=='f')
-                [TSVD.U, TSVD.S, TSVD.V] = t_SVD(varargin{1},varargin{2});
-                if isa(varargin{1},'tensor')
-                    TSVD.size = varargin{1}.size;
-                else
-                    TSVD.size = size(varargin{1});
-                end
-            else
+            if nargin~=3 
                 error("Incorrect input!");
+            elseif numel(size(varargin{1}))~=3 || numel(size(varargin{2}))~=3 || numel(size(varargin{3})）~=3
+                error("Incorrect dimensions of input!");
+            elseif size(varargin{1},2)~=size(varargin{2},1) || size(varargin{1},3)~=size(varargin{3},2) || size(varargin{2},2)~=size(varargin{3},2) || size(varargin{2},3)~=size(varargin{3},3)
+                error("Incorrect shapes of input!");
+            else
+                if isa(varargin{1},'tensor')
+                    TSVD.U = varargin{1};
+                else
+                    TSVD.U = tensor(varargin{1});
+                end
+                if isa(varargin{2},'tensor')
+                    TSVD.S = varargin{2};
+                else
+                    TSVD.S = tensor(varargin{2});
+                end
+                if isa(varargin{1},'tensor')
+                    TSVD.U = varargin{3};
+                else
+                    TSVD.U = tensor(varargin{3});
+                end
             end
             return;
         end
