c9d7c287902d776612c8d57ce124a859435ffaed
liujiani <Jianiliunier@gmail.com>
2023-12-06 15:58:58 +0800

    Update display.m

--- td/TKtensor/@TKtensor/display.m
+++ td/TKtensor/@TKtensor/display.m
@@ -1,14 +1,18 @@
 function display(T)
 
+if isempty(T.size)
+    fprintf('%s is a empty TKtensor',getVarName(T,inputname(1)));
+    return
+end
 
 fprintf(['%s is a TKtensor of size ',repmat('%dx',1,numel(T.size)-1), '%d', ' and rank ',repmat('%dx',1,numel(T.rank)-1), '%d.\n'],getVarName(T,inputname(1)),T.size(1:end-1),T.size(end),T.rank(1:end-1),T.rank(end));
 sz1 = size(T.core);
 fprintf(['core: [',repmat('%dx',1,numel(sz1)-1),'%d %s].\n'],sz1(1:end-1),sz1(end),class(T.core));
 sz=[];
 for i = 1:length(size(T.factors))
-sz = [sz size(T.factors{i})];
+    sz = [sz size(T.factors{i})];
 end
-        
+
 fprintf('factors: {');
 for i = 1:length(size(T.factors))
     sz = size(T.factors{i});
@@ -18,5 +22,5 @@ fprintf('}.\n');
 end
 
 function out = getVarName(~,name)
-    out = name;
+out = name;
 end
