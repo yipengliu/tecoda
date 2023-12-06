1696da508ca1b238533290a4abac8b19aa432486
liujiani <Jianiliunier@gmail.com>
2023-12-06 14:51:55 +0800

    Delete td/tttensor directory

--- td/tttensor/@tttensor/tt2tensor.m
+++
@@ -1,8 +0,0 @@
-function T = tt2tensor(TT)
-%convert a tttensor to a tensor
-    N =length(TT.factors);
-    T = TT.factors{1};
-    for i = 1:N-1
-        T = tensor_contraction(T,TT.factors{i+1},ndims(T),1);
-    end
-    T = squeeze(T);
