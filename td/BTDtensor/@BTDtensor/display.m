5ffc8ca272bf0c191d541465ffa4f86fbafa369c
liujiani <Jianiliunier@gmail.com>
2023-12-08 13:04:39 +0800

    Update display.m

--- td/BTDtensor/@BTDtensor/display.m
+++ td/BTDtensor/@BTDtensor/display.m
@@ -1,19 +1,23 @@
 function display(T)
 
 
-fprintf(['%s is a BTDtensor of size ',repmat('%dx',1,numel(T.size)-1), '%d', ' and R %d.\n'],getVarName(T,inputname(1)),T.size(1:end-1),T.size(end),T.R);
+if isempty(T.size)
+    fprintf('%s is a empty BTDtensor',getVarName(T,inputname(1)));
+    return;
+end
+fprintf(['%s is a BTDtensor of size ',repmat('%dx',1,numel(T.size)-1), '%d', ' and R %d.\n'],getVarName(T,inputname(1)),T.size(1:end-1),T.size(end),T.rank);
 fprintf('L: {');
-for r=1:T.R
+for r=1:T.rank
     sz_tmp=T.L{r};
     fprintf(['[',repmat('%d, ', 1, size(sz_tmp, 2)-1), '%d', ']  '],sz_tmp(1:end-1),sz_tmp(end));
 end
 fprintf('}.\n');
-for r=1:T.R
+for r=1:T.rank
     sz1=size(T.cores{r});
     fprintf(['core{%d}: [',repmat('%dx',1,numel(sz1)-1),'%d %s].\n'],r,sz1(1:end-1),sz1(end),class(T.cores{r}));
 end
 
-for r=1:T.R
+for r=1:T.rank
     sz=[];
     for i=1:length(size(T.factors{r}))
         sz = [sz size(T.factors{r}{i})];
@@ -28,5 +32,5 @@ end
 end
 
 function out = getVarName(~,name)
-    out = name;
+out = name;
 end
