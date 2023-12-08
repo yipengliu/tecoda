c6a64167705c84486b41786f287e5b9be3645098
liujiani <Jianiliunier@gmail.com>
2023-12-08 13:04:11 +0800

    Update BTDtensor.m

--- td/BTDtensor/@BTDtensor/BTDtensor.m
+++ td/BTDtensor/@BTDtensor/BTDtensor.m
@@ -6,34 +6,34 @@ classdef BTDtensor
         factors
         size
         L
-        R
+        rank
     end
     methods
         function BTD = BTDtensor(varargin)
-        % BTDtensor is BTD format tensor(decomposed).
-        % BTD = BTDtensor() creates a empty BTDtensor.
-        % BTD = BTDtensor(BTD) copy a BTDtensor.
-        % BTD = BTDtensor(U) use core tensor and factors fn to
-        % create a BTDtensor.
-        % BTD = BTDtensor(U)use a cell to
-        % create a BTDtensor,the cell contains all factors and all cores.
+            % BTDtensor is BTD format tensor(decomposed).
+            % BTD = BTDtensor() creates a empty BTDtensor.
+            % BTD = BTDtensor(BTD) copy a BTDtensor.
+            % BTD = BTDtensor(U) use core tensor and factors fn to
+            % create a BTDtensor.
+            % BTD = BTDtensor(U)use a cell to
+            % create a BTDtensor,the cell contains all factors and all cores.
             if (nargin == 0)
-                BTD.size = 0;
+                BTD.size = [];
                 BTD.cores = {};
                 BTD.factors = {};
                 BTD.L = {};
-                BTD.R = 0;
+                BTD.rank = [];
                 return;
             end
             if(nargin == 1 && isa(varargin{1},'BTDtensor'))
                 BTD = varargin{1};
                 return ;
             end
-            if ~isa(varargin{1},'cell')
-                error('U must be a cell.');
-            else
-                BTD.R = length(varargin{1});
-                for r = 1:BTD.R
+            
+            
+            if (nargin == 1 && isa(varargin{1},'cell'))
+                BTD.rank = length(varargin{1});
+                for r = 1:BTD.rank
                     BTD.cores{r} = tensor(varargin{1}{r}{1});
                     if isa(varargin{1}{r}{2},'cell')
                         BTD.factors{r} = varargin{1}{r}{2};
@@ -45,13 +45,29 @@ classdef BTDtensor
                     end
                 end
             end
+            if(nargin>1)
+                BTD.rank =nargin;
+                
+                for r = 1:BTD.rank
+                    BTD.cores{r} = tensor(varargin{r}{1});
+                    if isa(varargin{r}{2},'cell')
+                        BTD.factors{r} = varargin{r}{2};
+                    else
+                        N = length(varargin{r});
+                        for i = 2:N
+                            BTD.factors{r}{i-1} = varargin{r}{i};
+                        end
+                    end
+                end
+                
+            end
             %check that factors
             fac_num = length(BTD.factors{1});
             %get the BTDtensor size
             for dim = 1:fac_num
                 BTD.size(dim) = size(BTD.factors{1}{dim},1);
             end
-            for r = 1:BTD.R
+            for r = 1:BTD.rank
                 if length(BTD.factors{r}) ~= fac_num
                     error('all factors must have same length for the BTDtensor');
                 end
