26e9507bcd205dc955f64e23634c01567e3a2c88
liujiani <Jianiliunier@gmail.com>
2023-12-08 13:03:48 +0800

    Update BTD2tensor.m

--- td/BTDtensor/@BTDtensor/BTD2tensor.m
+++ td/BTDtensor/@BTDtensor/BTD2tensor.m
@@ -1,7 +1,7 @@
 function t=BTD2tensor(BTD)
     %convert a BTDtensor to a tensor.
     t=tensor('zeros',BTD.size);
-    for r=1:BTD.R
+    for r=1:BTD.rank
         t = calculate('add', t, mode_n_product(BTD.cores{r},BTD.factors{r},'T'));
     end
 end
